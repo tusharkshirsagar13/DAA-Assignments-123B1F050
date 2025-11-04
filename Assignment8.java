3f0a80549536a5dfa05cfa81f362b21c8dd4823a
Tushar Kshirsagar <156572338+tusharkshirsagar13@users.noreply.github.com>
2025-11-04 15:07:19 +0530

    Add files via upload

---
+++ Assignment8.java
@@ -0,0 +1,155 @@
+
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int level;             
+    int pathCost;          
+    int reducedCost;       
+    int vertex;            
+    List<Integer> path;    
+    int[][] reducedMatrix; 
+
+    Node(int n) {
+        reducedMatrix = new int[n][n];
+        path = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return Integer.compare(this.reducedCost, other.reducedCost);
+    }
+}
+
+public class Assignmnet8_DAA{
+    static final int INF = 9999999;
+
+    
+    static void copyMatrix(int[][] src, int[][] dest) {
+        for (int i = 0; i < src.length; i++)
+            System.arraycopy(src[i], 0, dest[i], 0, src.length);
+    }
+
+    
+    static int reduceMatrix(int[][] matrix, int n) {
+        int reductionCost = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            int rowMin = INF;
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] < rowMin)
+                    rowMin = matrix[i][j];
+
+            if (rowMin != INF && rowMin != 0) {
+                reductionCost += rowMin;
+                for (int j = 0; j < n; j++)
+                    if (matrix[i][j] != INF)
+                        matrix[i][j] -= rowMin;
+            }
+        }
+
+      
+        for (int j = 0; j < n; j++) {
+            int colMin = INF;
+            for (int i = 0; i < n; i++)
+                if (matrix[i][j] < colMin)
+                    colMin = matrix[i][j];
+
+            if (colMin != INF && colMin != 0) {
+                reductionCost += colMin;
+                for (int i = 0; i < n; i++)
+                    if (matrix[i][j] != INF)
+                        matrix[i][j] -= colMin;
+            }
+        }
+        return reductionCost;
+    }
+
+   
+    static Node createNode(int[][] parentMatrix, List<Integer> path, int level, int i, int j, int n) {
+        Node node = new Node(n);
+        copyMatrix(parentMatrix, node.reducedMatrix);
+
+        
+        for (int k = 0; level != 0 && k < n; k++)
+            node.reducedMatrix[i][k] = INF;
+
+        for (int k = 0; k < n; k++)
+            node.reducedMatrix[k][j] = INF;
+
+    
+        if (level + 1 < n)
+            node.reducedMatrix[j][0] = INF;
+
+        node.path.addAll(path);
+        node.path.add(j);
+        node.level = level;
+        node.vertex = j;
+        return node;
+    }
+
+    
+    static void solveTSP(int[][] costMatrix, int n) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+
+        Node root = new Node(n);
+        copyMatrix(costMatrix, root.reducedMatrix);
+
+        root.path = path;
+        root.level = 0;
+        root.vertex = 0;
+        root.pathCost = 0;
+        root.reducedCost = reduceMatrix(root.reducedMatrix, n);
+
+        pq.add(root);
+        int minCost = INF;
+        List<Integer> finalPath = new ArrayList<>();
+
+        while (!pq.isEmpty()) {
+            Node min = pq.poll();
+            int i = min.vertex;
+
+            if (min.level == n - 1) {
+               
+                min.path.add(0);
+                int totalCost = min.pathCost + costMatrix[i][0];
+                if (totalCost < minCost) {
+                    minCost = totalCost;
+                    finalPath = new ArrayList<>(min.path);
+                }
+                continue;
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (min.reducedMatrix[i][j] != INF) {
+                    Node child = createNode(min.reducedMatrix, min.path, min.level + 1, i, j, n);
+                    child.pathCost = min.pathCost + costMatrix[i][j];
+                    child.reducedCost = child.pathCost + reduceMatrix(child.reducedMatrix, n);
+                    pq.add(child);
+                }
+            }
+        }
+
+        System.out.println("\nOptimal Delivery Route (SwiftShip): " + finalPath);
+        System.out.println("Minimum Total Delivery Cost: " + minCost);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of cities: ");
+        int n = sc.nextInt();
+
+        int[][] costMatrix = new int[n][n];
+        System.out.println("Enter cost matrix (use large number for no direct route):");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                costMatrix[i][j] = sc.nextInt();
+
+        solveTSP(costMatrix, n);
+        sc.close();
+    }
+}
